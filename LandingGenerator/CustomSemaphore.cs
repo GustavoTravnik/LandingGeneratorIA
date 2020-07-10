def9389e44380699884657f3caa29c996e635578
gustavotravnik <gustavotravnik@outlook.com>
2020-07-10 04:40:35 -0300

    Added image list from google

---
+++ LandingGenerator/CustomSemaphore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandingGenerator
+{
+    public static class CustomSemaphore
+    {
+        public static bool IsFree { get; private set; }
+
+        public static void Lock()
+        {
+            IsFree = false;
+        }
+
+        public static void Unlock()
+        {
+            IsFree = true;
+        }
+    }
+}
